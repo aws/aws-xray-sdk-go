89001052516f60c9b3bebf297ce5f2a52ada1301
Yogiraj Awati <awati@amazon.com>
2019-09-03 14:12:40 -0700

    prepare for release 1.0.0-rc.14 (#146)
    * prepare for release 1.0.0-rc.14

--- xray/config.go
+++ xray/config.go
@@ -24,7 +24,7 @@ import (
 )
 
 // SDKVersion records the current X-Ray Go SDK version.
-const SDKVersion = "1.0.0-rc.13"
+const SDKVersion = "1.0.0-rc.14"
 
 // SDKType records which X-Ray SDK customer uses.
 const SDKType = "X-Ray for Go"
