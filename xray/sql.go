bf5395f45c1bafee61739efa97e5a998babb33ca
aircraft-cerier <58537442+aircraft-cerier@users.noreply.github.com>
2020-02-12 13:25:06 -0800

    fixing to cover MySQL use case for when no password is part of dsn. Also separating test cases for pre Go1.11 and Go 1.11 and later.  the Go team made some changes to URL parsing as a security fix in 1.13 and backported the change to 1.11.13 and 1.12.8.

--- xray/sql.go
+++ xray/sql.go
@@ -281,13 +281,15 @@ func stripPasswords(dsn string) string {
 				buf.UnreadByte()
 			}
 		case '@':
-			isPassword = true
-			flush()
-			resLen := res.Len()
-			if resLen > 0 && res.Bytes()[resLen-1] == ':' {
-				res.Truncate(resLen - 1)
+			if strings.Contains(res.String(), ":") {
+				resLen := res.Len()
+				if resLen > 0 && res.Bytes()[resLen-1] == ':' {
+					res.Truncate(resLen - 1)
+				}
+				isPassword = true
+				flush()
+				res.WriteByte(c)
 			}
-			res.WriteByte(c)
 		}
 	}
 	inBraces = false
