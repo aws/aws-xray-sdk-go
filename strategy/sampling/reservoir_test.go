e1ef6582afa25fd7f5001faea11a43fe5ef5acb1
Bhautik Pipaliya <bhautip@amazon.com>
2020-02-14 15:38:31 -0800

    fix: test 9

--- strategy/sampling/reservoir_test.go
+++ strategy/sampling/reservoir_test.go
@@ -26,7 +26,7 @@ func takeOverTime(r *Reservoir, millis int) int {
 		if r.Take() {
 			taken++
 		}
-		time.Sleep(103 * time.Millisecond)
+		time.Sleep(102 * time.Millisecond)
 	}
 	return taken
 }
