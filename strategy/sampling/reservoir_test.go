e5b1a365d70d629d1ec5bb340745ed63a3fd3260
bhautikpip <bhautip@amazon.com>
2020-03-23 11:55:48 -0700

    Added fix

--- strategy/sampling/reservoir_test.go
+++ strategy/sampling/reservoir_test.go
@@ -22,10 +22,12 @@ const Interval = 100
 func takeOverTime(r *Reservoir, millis int) int {
 	taken := 0
 	for i := 0; i < millis/Interval; i++ {
+		r.mu.Lock()
 		if r.Take() {
 			taken++
 		}
 		time.Sleep(Interval * time.Millisecond)
+		r.mu.Unlock()
 	}
 	return taken
 }
@@ -44,6 +46,7 @@ func TestOnePerSecond(t *testing.T) {
 	taken := takeOverTime(res, TestDuration)
 	assert.True(t, int(math.Ceil(TestDuration/1000.0)) <= taken)
 	assert.True(t, int(math.Ceil(TestDuration/1000.0))+cap >= taken)
+	// Try 4s
 }
 
 func TestTenPerSecond(t *testing.T) {
