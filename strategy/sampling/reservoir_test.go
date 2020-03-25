4dcc00591bc70dc4d2813fe1b1bccbe793e3db3b
bhautikpip <bhautip@amazon.com>
2020-03-25 13:15:44 -0700

    Trigger Git actions

--- strategy/sampling/reservoir_test.go
+++ strategy/sampling/reservoir_test.go
@@ -9,12 +9,11 @@
 package sampling
 
 import (
+	"github.com/aws/aws-xray-sdk-go/utils"
+	"github.com/stretchr/testify/assert"
 	"math"
 	"testing"
 	"time"
-
-	"github.com/aws/aws-xray-sdk-go/utils"
-	"github.com/stretchr/testify/assert"
 )
 
 const Interval = 100
@@ -33,7 +32,7 @@ func takeOverTime(r *Reservoir, millis int) int {
 const TestDuration = 1500
 
 func TestOnePerSecond(t *testing.T) {
-	clock := &utils.DefaultClock{}
+	clock := &utils.DefaultClock {}
 	cap := 1
 	res := &Reservoir{
 		clock: clock,
