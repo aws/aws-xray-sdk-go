f46b3cb81fa31974a99ccdbac23ac623a3b85a3b
jialiawu <jialiawu@amazon.com>
2020-10-28 21:00:43 -0700

    Add LICENSE file and change the License header

--- sample-apps/aws-xray-sdk-go-sample/main.go
+++ sample-apps/aws-xray-sdk-go-sample/main.go
@@ -1,5 +1,3 @@
-// Copyright 2017-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//
 // Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
 //
 //     http://aws.amazon.com/apache2.0/
@@ -20,8 +18,11 @@ import (
 )
 
 func webServer(){
+	http.Handle("/",http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("This is root"))
+	}))
 	http.Handle("/outgoing-http-call", xray.Handler(xray.NewFixedSegmentNamer("SampleApplication"), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := ctxhttp.Get(r.Context(), xray.Client(nil), "https://aws.amazon.com/")
+		_, err := ctxhttp.Get(r.Context(), xray.Client(nil), "https://aws.amazon.com")
 		if err != nil {
 			log.Println(err)
 			return
