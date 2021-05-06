1fefe669f7ba87b5c93168f0c105e28c4123f3bb
Nicolas Barbosa <ndevbarbosa@gmail.com>
2021-05-06 09:30:06 -0300

    chore: add fasthttp benchmark test

--- xray/fasthttp_test.go
+++ xray/fasthttp_test.go
@@ -14,6 +14,25 @@ func TestFastHTTPHandler(t *testing.T) {
 	cfg := GetRecorder(ctx1)
 	defer td.Close()
 
+	fh := NewFastHTTPInstrumentor(cfg)
+	handler := fh.Handler(NewFixedSegmentNamer("test"), func(ctx *fasthttp.RequestCtx) {})
+	rc := genericRequestCtx()
+	handler(rc)
+
+	seg, err := td.Recv()
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.Equal(t, http.StatusOK, rc.Response.StatusCode())
+	assert.Equal(t, http.MethodPost, seg.HTTP.Request.Method)
+	assert.Equal(t, "http://localhost/path", seg.HTTP.Request.URL)
+	assert.Equal(t, "1.2.3.5", seg.HTTP.Request.ClientIP)
+	assert.Equal(t, "UA_test", seg.HTTP.Request.UserAgent)
+}
+
+// genericRequestCtx helper function to build fasthttp.RequestCtx
+func genericRequestCtx() *fasthttp.RequestCtx {
 	b := `{"body": "content"}`
 	req := fasthttp.Request{}
 	req.SetBodyString(b)
@@ -33,18 +52,19 @@ func TestFastHTTPHandler(t *testing.T) {
 	}
 	rc.SetRemoteAddr(remoteAddr)
 
-	fh := NewFastHTTP(cfg)
+	return rc
+}
+
+func BenchmarkFasthttpHandler(b *testing.B) {
+	ctx1, td := NewTestDaemon()
+	cfg := GetRecorder(ctx1)
+	defer td.Close()
+
+	fh := NewFastHTTPInstrumentor(cfg)
 	handler := fh.Handler(NewFixedSegmentNamer("test"), func(ctx *fasthttp.RequestCtx) {})
-	handler(rc)
 
-	seg, err := td.Recv()
-	if !assert.NoError(t, err) {
-		return
+	for i := 0; i < b.N; i++ {
+		rc := genericRequestCtx()
+		handler(rc)
 	}
-
-	assert.Equal(t, http.StatusOK, rc.Response.StatusCode())
-	assert.Equal(t, http.MethodPost, seg.HTTP.Request.Method)
-	assert.Equal(t, "http://localhost/path", seg.HTTP.Request.URL)
-	assert.Equal(t, "1.2.3.5", seg.HTTP.Request.ClientIP)
-	assert.Equal(t, "UA_test", seg.HTTP.Request.UserAgent)
 }
