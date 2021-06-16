b0d24a5ed81e76c13912ef653636e5f09e754ddf
bhautikpip <bhautip@amazon.com>
2021-06-16 08:29:21 -0700

    export data model field

--- xray/segment_model.go
+++ xray/segment_model.go
@@ -66,7 +66,7 @@ type Segment struct {
 
 	// Children
 	Subsegments    []json.RawMessage `json:"subsegments,omitempty"`
-	rawSubsegments []*Segment
+	RawSubsegments []*Segment
 
 	// Configuration
 	Configuration *Config `json:"-"`
@@ -109,7 +109,7 @@ type ResponseData struct {
 
 // ServiceData provides the shape for unmarshalling service version.
 type ServiceData struct {
-	Version         string `json:"version,omitempty"`
+	Version        string `json:"version,omitempty"`
 	RuntimeVersion string `json:"runtime_version,omitempty"`
 	Runtime        string `json:"runtime,omitempty"`
 }
