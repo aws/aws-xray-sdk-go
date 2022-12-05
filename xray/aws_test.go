8db3f536deb8fc91044025a68838878e7556a8e8
Adam Shaw <atshaw@amazon.com>
2022-12-04 22:26:06 -0800

    Fixing test

--- xray/aws_test.go
+++ xray/aws_test.go
@@ -174,7 +174,7 @@ func testClientFailedConnection(ctx context.Context, td *TestDaemon, t *testing.
 	assert.Error(t, err)
 
 	seg, err := td.Recv()
-	if !assert.NoError(t, err) {
+	if assert.Error(t, err) {
 		return
 	}
 
