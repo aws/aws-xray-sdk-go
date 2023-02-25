b795be75b3b8f912cf1c0866317530de7c14d57a
Jonathan Lee <jjllee@amazon.com>
2023-02-24 18:15:31 -0800

    Update changelog for 1.8.1

--- xray/config.go
+++ xray/config.go
@@ -24,7 +24,7 @@ import (
 )
 
 // SDKVersion records the current X-Ray Go SDK version.
-const SDKVersion = "1.8.0"
+const SDKVersion = "1.8.1"
 
 // SDKType records which X-Ray SDK customer uses.
 const SDKType = "X-Ray for Go"
